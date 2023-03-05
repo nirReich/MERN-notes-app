7d3002db4ee587d7840f83273b6fcba6b3aa4038
nirReich <reich.nir@gmail.com>
2023-03-05 14:50:52 +0200

    mongo db connection and dotenv setup

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,11 +1,17 @@
-import express from "express";
-const app = express();
-const port = 5000;
+import app from "./app";
+import mongoose from "mongoose";
 
-app.get("/", (req, res) => {
-  res.send("hello world");
-});
+import env from "./utils/validateEnv";
 
-app.listen(port, () => {
-  console.log(`server running on port ${port}`);
-});
+const port = env.PORT;
+
+if (port)
+  mongoose
+    .connect(env.MONGO_CONNECTION_STRING)
+    .then(() => {
+      console.log("mongoose connected!");
+      app.listen(port, () => {
+        console.log(`server running on port ${port}`);
+      });
+    })
+    .catch(console.error);
