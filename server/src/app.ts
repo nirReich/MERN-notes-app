2a00b33ffebdfb1d12df30c3cb431583f0638e1d
nirReich <reich.nir@gmail.com>
2023-03-05 23:12:51 +0200

    get note and create note routes added

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,10 +1,25 @@
 import "dotenv/config";
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
+import notesRoutes from "./routes/notes";
+import morgan from "morgan";
 
 const app = express();
 
-app.get("/", (req, res) => {
-    res.send("hello world");
-  });
+app.use(morgan("dev"))
+app.use(express.json())
+app.use("/api/notes", notesRoutes)
+
+app.use((req, res, next) => {
+  next(Error("endpoint not found!"));
+});
+
+//error handler
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+app.use((error: unknown, req: Request, res: Response, next: NextFunction) => {
+  console.error(error);
+  let errorMsg = `unknown error occurred!`;
+  if (error instanceof Error) errorMsg = error.message;
+  res.status(500).json({ error: errorMsg });
+});
 
 export default app;
---
+++ server/src/routes/notes.ts
@@ -0,0 +1,9 @@
+import express from "express";
+import * as NotesController from "../controllers/notes";
+
+const router = express.Router();
+router.get("/", NotesController.getNotes);
+router.get("/:noteId", NotesController.getSingleNote)
+router.post("/", NotesController.crateNote);
+
+export default router;
