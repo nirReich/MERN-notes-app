a0c4b218c41ebc1ec99654caad3b8c40e86056a7
nirReich <reich.nir@gmail.com>
2023-03-06 08:58:38 +0200

    http error handling

--- server/src/app.ts
+++ server/src/app.ts
@@ -2,15 +2,16 @@ import "dotenv/config";
 import express, { NextFunction, Request, Response } from "express";
 import notesRoutes from "./routes/notes";
 import morgan from "morgan";
+import createHttpError, { isHttpError } from "http-errors";
 
 const app = express();
 
-app.use(morgan("dev"))
-app.use(express.json())
-app.use("/api/notes", notesRoutes)
+app.use(morgan("dev"));
+app.use(express.json());
+app.use("/api/notes", notesRoutes);
 
 app.use((req, res, next) => {
-  next(Error("endpoint not found!"));
+  next(createHttpError(404, "endpoint not found!"));
 });
 
 //error handler
@@ -18,8 +19,12 @@ app.use((req, res, next) => {
 app.use((error: unknown, req: Request, res: Response, next: NextFunction) => {
   console.error(error);
   let errorMsg = `unknown error occurred!`;
-  if (error instanceof Error) errorMsg = error.message;
-  res.status(500).json({ error: errorMsg });
+  let errorStatus = 500;
+  if (isHttpError(error)) {
+    errorStatus = error.status;
+    errorMsg = error.message;
+  }
+  res.status(errorStatus).json({ error: errorMsg });
 });
 
 export default app;
