beb85181526a3f36cd0c815f5d94bc45abd00565
nirReich <reich.nir@gmail.com>
2023-03-09 13:41:17 +0200

    add note modal and form

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
-import { Col, Container, Row } from "react-bootstrap";
+import { Button, Col, Container, Row } from "react-bootstrap";
+import AddNoteDialog from "./components/addNoteDialog/AddNoteDialog";
 import Note from "./components/note/Note";
 import { Note as NoteModel } from "./models/notes";
 import * as NotesApi from "./network/notes_api";
@@ -7,10 +8,11 @@ import styles from "./styles/NotesPage.module.css";
 
 function App() {
   const [notes, setNotes] = useState<NoteModel[]>([]);
+  const [showAddModal, setShowAddModal] = useState(false);
   useEffect(() => {
     async function loadNotes() {
       try {
-        const notes = await NotesApi.fetchNotes()
+        const notes = await NotesApi.fetchNotes();
         setNotes(notes);
       } catch (error) {
         console.error(error);
@@ -22,6 +24,7 @@ function App() {
 
   return (
     <Container>
+      <Button onClick={() => setShowAddModal(true)}>Add New Note</Button>
       <Row xs={1} md={2} xl={3} className="g-4">
         {notes.map((element, index) => {
           return (
@@ -31,6 +34,15 @@ function App() {
           );
         })}
       </Row>
+      {showAddModal && (
+        <AddNoteDialog
+          onDismiss={() => setShowAddModal(false)}
+          onNoteSaved={(newNote) => {
+            setNotes([...notes, newNote]);
+            setShowAddModal(false);
+          }}
+        />
+      )}
     </Container>
   );
 }
--- client/src/network/notes_api.ts
+++ client/src/network/notes_api.ts
@@ -1,5 +1,10 @@
 import { Note } from "../models/notes";
 
+export interface NoteInput {
+  title: string;
+  text?: string;
+}
+
 async function fetchData(input: RequestInfo, init?: RequestInit) {
   const res = await fetch(input, init);
   if (res.ok) {
@@ -7,13 +12,24 @@ async function fetchData(input: RequestInfo, init?: RequestInit) {
   } else {
     const errorBody = await res.json();
     const errMsg = errorBody.error;
-    throw Error(errMsg)
+    throw Error(errMsg);
   }
 }
 
 export async function fetchNotes(): Promise<Note[]> {
-    const res = await fetchData("/api/notes", {
-        method: "GET",
-      });
-      return await res.json();
+  const res = await fetchData("/api/notes", {
+    method: "GET",
+  });
+  return await res.json();
+}
+
+export async function createNote(note: NoteInput): Promise<Note> {
+  const res = await fetchData("api/notes", {
+    method: "POST",
+    headers: {
+      "Content-type": "application/json",
+    },
+    body: JSON.stringify(note),
+  });
+  return res.json()
 }
